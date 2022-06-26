8e50b63d7e231bd943f0deef5a2e5a498a6ffaf5
BrendonW250 <brendonwright16@gmail.com>
2022-06-25 21:33:59 -0400

    changes pt3

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,61 @@
 // client side js
 document.querySelector('button').addEventListener('click', getFetch)
 
+const myFavs = document.querySelector('input').value
+const url = `https://fav-movie-api.herokuapp.com/api/${myFavs}`
 
-async function getFetch(){
-    const myFavs = document.querySelector('input').value
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
 
-    try{
-        const response = await fetch(`https://fav-movie-api.herokuapp.com/api/${myFavs}`)
-        const data = await response.json()
+            // for I Am Legend
+            document.querySelector('.name-of-movie').innerHTML = 'Movie: ' + data.movieTitle
 
-        console.log(data)
+            // date released
+            document.querySelector('.dateReleased').innerHTML = 'Release Date: ' + data.dateReleased
+            // Celebs in movie
+            document.querySelector('.celeb1').innerHTML = 'Name: ' + data.notableCelebs.name1
+            document.querySelector('.celeb2').innerHTML = 'Name: ' + data.notableCelebs.name2
+            document.querySelector('.celeb3').innerHTML = 'Name: ' + data.notableCelebs.name3
 
-         // for I Am Legend
-         document.querySelector('.name-of-movie').innerHTML = 'Movie: ' + data.movieTitle
+            // for short summary of movie
+            document.querySelector('.movie-summary').innerHTML = 'Summary Of Movie: ' + data.shortBio
 
-         // date released
-         document.querySelector('.dateReleased').innerHTML = 'Release Date: ' + data.dateReleased
-         // Celebs in movie
-         document.querySelector('.celeb1').innerHTML = 'Name: ' + data.notableCelebs.name1
-         document.querySelector('.celeb2').innerHTML = 'Name: ' + data.notableCelebs.name2
-         document.querySelector('.celeb3').innerHTML = 'Name: ' + data.notableCelebs.name3
+            // for movie rating 
+            document.querySelector('.movie-rating').innerHTML = 'Rotten Tomatoes Rating: ' + data.rottenTomatoesRating
+        })
 
-         // for short summary of movie
-         document.querySelector('.movie-summary').innerHTML = 'Summary Of Movie: ' + data.shortBio
+        .catch(err => {
+            console.log(`error ${err}`)
+        }) 
 
-         // for movie rating 
-         document.querySelector('.movie-rating').innerHTML = 'Rotten Tomatoes Rating: ' + data.rottenTomatoesRating
-    }catch(error){
-        console.log(error)
-    }
-}
+
+
+// async function getFetch(){
+//     const myFavs = document.querySelector('input').value
+
+//     try{
+//         const response = await fetch(`https://fav-movie-api.herokuapp.com/api/${myFavs}`)
+//         const data = await response.json()
+
+//         console.log(data)
+
+//          // for I Am Legend
+//          document.querySelector('.name-of-movie').innerHTML = 'Movie: ' + data.movieTitle
+
+//          // date released
+//          document.querySelector('.dateReleased').innerHTML = 'Release Date: ' + data.dateReleased
+//          // Celebs in movie
+//          document.querySelector('.celeb1').innerHTML = 'Name: ' + data.notableCelebs.name1
+//          document.querySelector('.celeb2').innerHTML = 'Name: ' + data.notableCelebs.name2
+//          document.querySelector('.celeb3').innerHTML = 'Name: ' + data.notableCelebs.name3
+
+//          // for short summary of movie
+//          document.querySelector('.movie-summary').innerHTML = 'Summary Of Movie: ' + data.shortBio
+
+//          // for movie rating 
+//          document.querySelector('.movie-rating').innerHTML = 'Rotten Tomatoes Rating: ' + data.rottenTomatoesRating
+//     }catch(error){
+//         console.log(error)
+//     }
