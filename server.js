03a01500d6908afe4980467e1c9dd1868ef031fa
BrendonW250 <brendonwright16@gmail.com>
2022-07-05 17:26:02 -0400

    change6

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = 8000
-const connectionString = 'mongodb+srv://beazy250:thelegitapp24@cluster0.pyehc.mongodb.net/?retryWrites=true&w=majority'
+// const connectionString = 'mongodb+srv://beazy250:thelegitapp24@cluster0.pyehc.mongodb.net/?retryWrites=true&w=majority'
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
 // let db,
-// let dbconnectionStr = process.env.DB_STRING
+let dbconnectionStr = process.env.DB_STRING
 //     dbName = 'favMoviesPt2'
 
 
@@ -25,7 +25,7 @@ app.use(express.static('public'))
 
 
 
-MongoClient.connect(connectionString)
+MongoClient.connect(dbconnectionStr)
     .then(client => {
         // console.log(`Connected to ${dbname} Database`)
         // db = client.db(dbName)
